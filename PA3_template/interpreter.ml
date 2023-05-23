8df28c408169c34b59fd5f33ce1d8d3235148049
SincerityHun <120380703+SincerityHun@users.noreply.github.com>
2023-05-23 15:21:56 +0900

    Change Interpreter

--- PA3_template/interpreter.ml
+++ PA3_template/interpreter.ml
@@ -2,26 +2,27 @@ module F = Format
 
 (* Interpret an expression *)
 let rec interp_expr (e: Ast.expr) (g: FStore.t) (s: Store.t) : Value.t = 
+  (* Function for errors *)
   let error id = failwith ("Free identifier: " ^ id) in
   let function_error fn = failwith ("Undefined function: " ^ fn) in
   let arg_mismatch_error fn req act = 
     failwith ("The number of arguments of " ^ fn ^ " mismatched: Required: " ^ string_of_int req ^ ", Actual: " ^ string_of_int act) in
   match e with
-  | Ast.Num n -> Value.NumV n
-  | Ast.Add (e1, e2) -> 
+  | Ast.Add (e1, e2) -> (* Addition operation *)
     (match (interp_expr e1 g s, interp_expr e2 g s) with
-    | (Value.NumV v1, Value.NumV v2) -> Value.NumV (v1 + v2))
-  | Ast.Sub (e1, e2) -> 
+    | (Value.NumV n1, Value.NumV n2) -> Value.NumV (n1 + n2))
+  | Ast.Sub (e1, e2) -> (* Subtraction operation *)
     (match (interp_expr e1 g s, interp_expr e2 g s) with
-    | (Value.NumV v1, Value.NumV v2) -> Value.NumV (v1 - v2))
-  | Ast.Id x -> 
+    | (Value.NumV n1, Value.NumV n2) -> Value.NumV (n1 - n2))
+  | Ast.Id x -> (* Identifier interpretations *)
     if Store.mem x s then Store.find x s
     else error x
-  | Ast.LetIn (x, e1, e2) -> 
-    let v1 = interp_expr e1 g s in
-    let s' = Store.add x v1 s in
+  | Ast.Num n -> Value.NumV n (* Number Value *)
+  | Ast.LetIn (x, e1, e2) -> (* LetIn expression interpretations *)
+    let n1 = interp_expr e1 g s in
+    let s' = Store.add x n1 s in
     interp_expr e2 g s'
-  | Ast.Call (f, args) -> 
+  | Ast.Call (f, args) -> (* Function call interpretation *) 
     if FStore.mem f g then
       let (params, body) = FStore.find f g in
       if List.length params = List.length args then
